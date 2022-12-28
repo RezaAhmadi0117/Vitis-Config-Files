c7a4af7150b49b845c662e38cdab3ab01510db5b
jack <39849270+jackwatson01234@users.noreply.github.com>
2022-12-28 23:32:13 +0330

    First Commit

---
+++ vitis_var.sh
@@ -0,0 +1,10 @@
+
+
+source /tools/Xilinx/Vitis/2021.1/settings64.sh
+source /opt/xilinx/xrt/setup.sh
+export XILINX_VITIS=/tools/Xilinx/Vitis/2021.1
+
+export PATH=$PATH:/opt/xilinx/xrt/bin
+export XILINX_XRT=/opt/xilinx/xrt
+
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
